496457a8b5520dd15db2d3a8bf35073718c2f607
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-10 04:13:38 +0530

    Add 2_map.js

---
+++ 2_map.js
@@ -0,0 +1,11 @@
+const animals = [
+  { name: 'Fluff', species: 'rabbit' },
+  { name: 'Caro', species: 'dog' },
+  { name: 'Hamilton', species: 'dog' },
+  { name: 'Harold', species: 'fish' },
+  { name: 'Ursula', species: 'cat' },
+  { name: 'Jimmy', species: 'fish' },
+]
+
+const names = animals.map((animal) => animal.name)
+console.log(names)
